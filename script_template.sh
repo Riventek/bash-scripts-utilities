625110351dd8e40f23d3bf14d3754ef41a115e03
Unknown <franky@riventek.com>
2017-03-11 11:37:35 +0100

    - Add Terminal color codes as variables to facilitate make things nicer :)

--- script_template.sh
+++ script_template.sh
@@ -26,6 +26,7 @@
 #D    [2] http://wiki.bash-hackers.org/scripting/style
 #D    [3] http://bash3boilerplate.sh/
 #D    [4] http://www.tldp.org/LDP/abs/html/abs-guide.html
+#D    [5] http://misc.flogisoft.com/bash/tip_colors_and_formatting
 #D
 
 #########################################################################
@@ -51,6 +52,54 @@ readonly TMPFILE=$(mktemp)      # Generate the temporary mask
 # Add the commands and libraries required for the script to run
 RK_DEPENDENCIES="sed grep date"    
 RK_LIBRARIES="required_library.sh"
+
+# CODES FOR TERMINAL - Optimized for compatibility - use printf also for better compatibility
+# Styles
+readonly BOLD="\e[1m"
+readonly BOLD_OFF="\e[21m"
+readonly UNDERLINE="\e[4m"
+readonly UNDERLINE_OFF="\e[24m"
+readonly REVERSE="\e[7m"
+readonly REVERSE_OFF="\e[27m"
+readonly STYLE_OFF="\e[0m"
+
+# Colors Foreground
+readonly FG_BLACK="\e[30m"
+readonly FG_RED="\e[31m"
+readonly FG_GREEN="\e[32m"
+readonly FG_YELLOW="\e[33m"
+readonly FG_BLUE="\e[34m"
+readonly FG_MAGENTA="\e[35m"
+readonly FG_CYAN="\e[36m"
+readonly FG_LIGHTGREY="\e[37m"
+readonly FG_DARKGREY="\e[90m"
+readonly FG_LIGHTRED="\e[91m"
+readonly FG_LIGHTGREEN="\e[92m"
+readonly FG_LIGHTYELLOW="\e[93m"
+readonly FG_LIGHTBLUE="\e[94m"
+readonly FG_LIGHTMAGENTA="\e[95m"
+readonly FG_LIGHTCYAN="\e[96m"
+readonly FG_WHITE="\e[97m"
+readonly FG_DEFAULT="\e[39m"
+
+# Colors Background
+readonly BG_BLACK="\e[40m"
+readonly BG_RED="\e[41m"
+readonly BG_GREEN="\e[42m"
+readonly BG_YELLOW="\e[43m"
+readonly BG_BLUE="\e[44m"
+readonly BG_MAGENTA="\e[45m"
+readonly BG_CYAN="\e[46m"
+readonly BG_LIGHTGREY="\e[47m"
+readonly BG_DARKGREY="\e[100m"
+readonly BG_LIGHTRED="\e[101m"
+readonly BG_LIGHTGREEN="\e[102m"
+readonly BG_LIGHTYELLOW="\e[103m"
+readonly BG_LIGHTBLUE="\e[104m"
+readonly BG_LIGHTMAGENTA="\e[105m"
+readonly BG_LIGHTCYAN="\e[106m"
+readonly BG_WHITE="\e[107m"
+readonly BG_DEFAULT="\e[49m"
 
 #########################################################################
 # BASIC FUNCTIONS FOR ALL SCRIPTS
@@ -134,7 +183,7 @@ function_name()
   fi
 
   # Main function code
-  
+
   # Cleaning up 
   rm -Rf ${tmpfile}*  
 }
